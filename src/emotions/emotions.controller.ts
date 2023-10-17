aff1441bf97a9536adc0741e21044b96fd7313dd
devwuu <jdw0477@gmail.com>
2023-10-17 10:13:01 +0900

    [docs] 감정 api docs 작성

--- src/emotions/emotions.controller.ts
+++ src/emotions/emotions.controller.ts
@@ -17,7 +17,19 @@ import { OnlyPrivateInterceptor } from '../common/interceptors/only-private.inte
 import { JwtAuthGuard } from '../auth/jwt.guard';
 import { User } from '../common/decorators/user.decorator';
 import { UserDto } from '../users/dto/user.dto';
+import {
+  ApiHeader,
+  ApiOperation,
+  ApiParam,
+  ApiResponse,
+} from '@nestjs/swagger';
+import { EmotionDto } from './dto/emotion.dto';
 
+@ApiHeader({
+  name: 'Authorization',
+  description: 'jwt token',
+  required: true,
+})
 @Controller('emotions')
 @UseGuards(JwtAuthGuard)
 @UseInterceptors(OnlyPrivateInterceptor)
@@ -26,26 +38,113 @@ export class EmotionsController {
 
   constructor(private readonly emotionsService: EmotionsService) {}
 
+  @ApiOperation({
+    summary: '감정 등록',
+  })
+  @ApiResponse({
+    status: 201,
+    description: '감정 등록 성공',
+    schema: {
+      properties: {
+        emotionId: {
+          type: 'string',
+          description: '등록된 감정 id',
+          example: 'a34ca11e-d39f-4541-b67d-983b242b0783',
+        },
+      },
+    },
+  })
   @Post()
-  create(@User() user: UserDto, @Body() createEmotionDto: CreateEmotionDto) {
-    return this.emotionsService.create(user.id, createEmotionDto);
+  async create(
+    @User() user: UserDto,
+    @Body() createEmotionDto: CreateEmotionDto,
+  ) {
+    const emotionId = await this.emotionsService.create(
+      user.id,
+      createEmotionDto,
+    );
+    return { emotionId };
   }
 
+  @ApiOperation({
+    summary: '등록된 감정 조회',
+  })
+  @ApiResponse({
+    status: 200,
+    type: EmotionDto,
+    isArray: true,
+  })
   @Get()
   findAll(@User() user: UserDto) {
     return this.emotionsService.findAll(user.id);
   }
 
+  @ApiOperation({
+    summary: '감정 상세 조회',
+  })
+  @ApiResponse({
+    status: 200,
+    type: EmotionDto,
+  })
+  @ApiParam({
+    required: true,
+    description: '조회할 감정 id',
+    name: 'id',
+  })
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.emotionsService.findOneById(id);
   }
 
+  @ApiOperation({
+    summary: '등록된 감정 수정',
+  })
+  @ApiResponse({
+    status: 200,
+    schema: {
+      properties: {
+        emotionId: {
+          type: 'string',
+          description: '수정한 감정 id',
+          example: 'a34ca11e-d39f-4541-b67d-983b242b0783',
+        },
+      },
+    },
+  })
+  @ApiParam({
+    required: true,
+    description: '수정할 감정 id',
+    name: 'id',
+  })
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateEmotionDto: UpdateEmotionDto) {
-    return this.emotionsService.update(id, updateEmotionDto);
+  async update(
+    @Param('id') id: string,
+    @Body() updateEmotionDto: UpdateEmotionDto,
+  ) {
+    const emotionId = await this.emotionsService.update(id, updateEmotionDto);
+    return { emotionId };
   }
 
+  @ApiOperation({
+    summary: '등록된 감정 삭제',
+  })
+  @ApiResponse({
+    status: 200,
+    schema: {
+      properties: {
+        emotionId: {
+          type: 'string',
+          description: '삭제된 감정 id',
+          example: 'a34ca11e-d39f-4541-b67d-983b242b0783',
+        },
+      },
+    },
+  })
+  @ApiParam({
+    required: true,
+    description: '삭제할 감정 id',
+    name: 'id',
+  })
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.emotionsService.remove(id);
--- src/emotions/entities/emotion.entity.ts
+++ src/emotions/entities/emotion.entity.ts
@@ -2,11 +2,17 @@ import { Column, Entity, JoinColumn, ManyToOne } from 'typeorm';
 import { CommonEntity } from '../../common/entities/common.entity';
 import { IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
 import { User } from '../../users/entities/user.entity';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity({
   name: 'EMOTION',
 })
 export class Emotion extends CommonEntity {
+  @ApiProperty({
+    description: '감정 이름',
+    example: '기쁨',
+    required: true,
+  })
   @Column({
     nullable: false,
   })
@@ -14,6 +20,11 @@ export class Emotion extends CommonEntity {
   @IsNumber()
   name: string;
 
+  @ApiProperty({
+    description: '비고',
+    example: '기쁜 감정',
+    required: false,
+  })
   @Column({
     nullable: true,
   })
@@ -21,6 +32,11 @@ export class Emotion extends CommonEntity {
   @IsOptional()
   etc?: string;
 
+  @ApiProperty({
+    description: '감정을 등록한 user id',
+    example: '94f3c50c-05e7-4b83-b311-1fb4cf84b3a1',
+    required: true,
+  })
   @ManyToOne(() => User, (user) => user.id, {
     cascade: true,
     onDelete: 'SET NULL',
